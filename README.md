f9c9f4570c4d7985f0a7063ceda48584b2193fcd
David Zambrano <dzambrano.226@gmail.com>
2021-07-08 18:10:34 -0500

    :sparkles: Finishing the desing in desktop and mobile

---
+++ README.md
@@ -0,0 +1,6 @@
+# Home Template
+
+Template to e-commerce shop develop with Html, Css and JavaScript.
+
+
+> Developed with all ❤ by David Zambrano
--- products.js
+++ products.js
@@ -1,13 +1,72 @@
-import * as data from "./data-grid.json";
+const dataProducts = {
+    "products": [
+        {
+            "title": "Custom Tilte 1",
+            "Subtitle": "Custom Subtitle 1",
+            "buttonText": "Redirect"
+        },
+        {
+            "title": "Custom Tilte 2",
+            "Subtitle": "Custom Subtitle 2",
+            "buttonText": "Redirect"
+        },
+        {
+            "title": "Custom Tilte 3",
+            "Subtitle": "Custom Subtitle 3",
+            "buttonText": "Redirect"
+        },
+        {
+            "title": "Custom Tilte 4",
+            "Subtitle": "Custom Subtitle 4",
+            "buttonText": "Redirect"
+        },
+        {
+            "title": "Custom Tilte 5",
+            "Subtitle": "Custom Subtitle 5",
+            "buttonText": "Redirect"
+        },
+        {
+            "title": "Custom Tilte 6",
+            "Subtitle": "Custom Subtitle 6",
+            "buttonText": "Redirect"
+        }
+    ]
+}
 
 const buildComponents = () => {
-    console.log('Hello world');
+    const { products } = dataProducts
+
     const divElement = document.querySelector('#product_cards');
-    // console.log(dataProducts);
-    console.log(divElement);
-    const elementToInsert = document.createElement("div");
-    elementToInsert.setAttribute('class', 'product');
-    divElement.appendChild(elementToInsert);
+    
+    for (let index = 0; index < products.length; index++) {
+        let element = document.createElement("div");
+        element.setAttribute('class', 'card');
+        element.setAttribute('id', `card-content-${index}`);
+        divElement.appendChild(element);
+
+        let cardElement = document.querySelector(`#card-content-${index}`);
+    
+        let contentElement = document.createElement("div");
+        contentElement.setAttribute('class', 'content');
+
+        const { title, Subtitle } = products[index];
+        console.log(title, Subtitle);
+
+        let titleElement = document.createElement("h3");
+        titleElement.appendChild( document.createTextNode(title));
+
+        let subTitleElement = document.createElement("p");
+        subTitleElement.appendChild( document.createTextNode(Subtitle));
+
+        contentElement.appendChild(titleElement);
+        contentElement.appendChild(document.createElement("hr"));
+        contentElement.appendChild(subTitleElement);
+
+    
+        cardElement.appendChild(contentElement);
+    }
+
+
     
 }
 
